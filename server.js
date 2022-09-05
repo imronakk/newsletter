5fe93f2937ec7f3a155ace0958e42f5b155b8c70
Ronak <ronakcode11@gmail.com>
2022-09-05 18:50:56 +0530

    mainCommit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const request = require('request');
+require("dotenv").config();
+
+const app = express();
+app.use(express.static("PUBLIC"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html")
+})
+
+app.post("/",function(req,res){
+    const first = req.body.firstName;
+    const Last = req.body.lastName;
+    const email = req.body.emaill;
+    
+    const data2 = {
+        members : [{
+            email_address: email,
+            status:"subscribed",
+            merge_fields:{
+                FNAME:first,
+                LNAME:Last,
+            }
+        }]
+    };
+    const JSONdata = JSON.stringify(data2);
+
+    const api = process.env.APIkey;
+    const urlMain = process.env.Endpointurl;
+    const url = urlMain;
+    const option = {
+        method:'POST',
+        auth: 'Ronak:'+api
+        
+    }
+    const request = https.request(url , option , function(response){
+        if(response.statusCode==200){
+            res.sendFile(__dirname + "/success.html")
+        }else{
+            res.sendFile(__dirname + "/faliure.html")
+        }
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        }) 
+    })
+    request.write(JSONdata);
+    request.end();
+})
+
+app.listen(process.env.PORT || 4000,function(){
+    console.log("Server is live")
+})
+
+app.post("/faliure",function(req,res){
+    res.redirect("/")
+})
+// MYAPI
+// f8c97d5613ddf4f72bb091bf19afd0ad-us12
+
+// AUDIENCE ID
+// 0f5f5af978
